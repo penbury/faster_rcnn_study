7e81aa3ad70b0cac0357577978505f5cbf4102e7
liuting <liuting@datu-medical.com>
2019-01-24 18:32:31 +0800

    finish generate anchors on the whole picture.

--- generate_anchors.py
+++ generate_anchors.py
@@ -5,15 +5,20 @@
 
 import numpy as np
 
+def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
+                     scales=[8, 16, 32]):
 
-def generate_anchors(base_size=2, ratios=[1, 2],
-                     scales=[1, 2, 4]):
-    # base_size 感知野的大小
-    # ratios 宽高比为1,2
-    # scales 面积分别为1*4,2*4,4*4
-    base_anchor = np.array([1, 1, base_size, base_size]) - 1 #[0,0,1,1]
-    w,h,x_ctr,y_ctr = _whctrs(base_anchor) # 2,2,0.5,0.5
+    base_anchor = np.array([1, 1, base_size, base_size]) - 1
     ratio_anchors = _ratio_enum(base_anchor, ratios)
+    print(ratio_anchors)
+    anchors = None
+    for i in range(len(scales)):
+        if anchors is None:
+            anchors = ratio_anchors * scales[i]
+        else:
+            anchors = np.vstack((anchors, ratio_anchors * scales[i]))
+    w, h, x_ctr, y_ctr = _whctrs(base_anchor)
+    anchors = _mkanchors(anchors[:, 0], anchors[:, 1], x_ctr, y_ctr)
     return anchors
 
 
@@ -25,9 +30,7 @@ def _whctrs(anchor):
     y_ctr = anchor[1] + 0.5 * (h - 1)
     return w, h, x_ctr, y_ctr
 
-
 def _mkanchors(ws, hs, x_ctr, y_ctr):
-
     ws = ws[:, np.newaxis]
     hs = hs[:, np.newaxis]
     anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
@@ -36,37 +39,17 @@ def _mkanchors(ws, hs, x_ctr, y_ctr):
                          y_ctr + 0.5 * (hs - 1)))
     return anchors
 
-
 def _ratio_enum(anchor, ratios):
-    # 获取中心点坐标及宽高 此处为(0.5,0.5) 宽=高=2
+    # 计算面积为感知野的大小时，宽高应是多少
     w, h, x_ctr, y_ctr = _whctrs(anchor)
     size = w * h
     size_ratios = size / ratios
     ws = np.round(np.sqrt(size_ratios))
     hs = np.round(ws * ratios)
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
-
-
-def _scale_enum(anchor, scales):
-    """
-    Enumerate a set of anchors for each scale wrt an anchor.
-    """
-
-    w, h, x_ctr, y_ctr = _whctrs(anchor)
-    ws = w * scales
-    hs = h * scales
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
-
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    ratios_anchors = np.hstack((ws, hs))
+    return ratios_anchors
 
 if __name__ == '__main__':
-    import time
-
-    t = time.time()
     a = generate_anchors()
-    print(time.time() - t)
-    print(a)
-    from IPython import embed;
-
-    embed()
